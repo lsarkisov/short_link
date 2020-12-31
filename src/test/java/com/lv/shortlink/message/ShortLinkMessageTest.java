5674c037208cdcbc5224d4dc9b07d29c2a9b4b53
coder <coder@coders-MacBook-Pro.local>
2020-12-31 08:22:54 +0100

    updated tests for messages

--- src/test/java/com/lv/shortlink/message/ShortLinkMessageTest.java
+++ src/test/java/com/lv/shortlink/message/ShortLinkMessageTest.java
@@ -1,17 +1,17 @@
 package com.lv.shortlink.message;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 class ShortLinkMessageTest {
 
     @Test
     void showTest() {
         Assertions.assertEquals(ShortLinkMessage.welcomeMessage, ShortLinkMessage.show(MessageTypes.WELCOME_MESSAGE));
+        Assertions.assertEquals(ShortLinkMessage.argumentValid, ShortLinkMessage.show(MessageTypes.ARGUMENTS_VALID));
+        Assertions.assertEquals(ShortLinkMessage.urlValid, ShortLinkMessage.show(MessageTypes.URL_VALID));
+        Assertions.assertEquals(ShortLinkMessage.keywordValid, ShortLinkMessage.show(MessageTypes.KEYWORD_VALID));
+        Assertions.assertEquals(
+                String.format("%stest%s", ShortLinkMessage.shorLinkExists1, ShortLinkMessage.shorLinkExists2), ShortLinkMessage.show(MessageTypes.SHORT_LINK_EXISTS, "test"));
     }
 }
