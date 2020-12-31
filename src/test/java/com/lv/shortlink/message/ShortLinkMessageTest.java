86cbd339b9f88a49b583e642db25cfc87a7a8171
coder <coder@coders-MacBook-Pro.local>
2020-12-31 08:10:10 +0100

    fixed test for message

--- src/test/java/com/lv/shortlink/message/ShortLinkMessageTest.java
+++ src/test/java/com/lv/shortlink/message/ShortLinkMessageTest.java
@@ -9,22 +9,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 class ShortLinkMessageTest {
-    private final PrintStream standardOut = System.out;
-    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-
-    @BeforeEach
-    public void init() {
-        System.setOut(new PrintStream(outputStreamCaptor));
-    }
-
-    @AfterEach
-    public void clean() {
-        System.setOut(standardOut);
-    }
 
     @Test
     void showTest() {
-        ShortLinkMessage.show(MessageTypes.WELCOME_MESSAGE);
-        Assertions.assertEquals("\nHi! Please, add url <http://mysite.com> or <https://mysite.com> and/or keyword.\nThe keyword should be alphanumeric and contain 5 symbols:\n\n", outputStreamCaptor.toString());
+        Assertions.assertEquals("\nHi! Please, add url <http://mysite.com> or <https://mysite.com> and/or keyword.\nThe keyword should be alphanumeric and contain 5 symbols:\n", ShortLinkMessage.show(MessageTypes.WELCOME_MESSAGE));
     }
 }
