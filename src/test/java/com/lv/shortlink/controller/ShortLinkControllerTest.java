5287a05eb7c496d1e544f98d6180bfd7910dea7f
coder <coder@coders-MacBook-Pro.local>
2021-01-02 11:43:38 +0100

    tests for controller

--- src/test/java/com/lv/shortlink/controller/ShortLinkControllerTest.java
+++ src/test/java/com/lv/shortlink/controller/ShortLinkControllerTest.java
@@ -1,21 +1,31 @@
 package com.lv.shortlink.controller;
 
+import com.lv.shortlink.message.ShortLinkMessage;
+import com.lv.shortlink.service.ShortLinkService;
 import org.junit.jupiter.api.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 
 class ShortLinkControllerTest {
     private static String url;
-    private final PrintStream standardOut = System.out;
-    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private final InputStream systemIn = System.in;
+    private final PrintStream systemOut = System.out;
 
     private ByteArrayInputStream testIn;
+    private ByteArrayOutputStream testOut;
 
     @BeforeAll
     public static void all() {
         url = "https://mysite.com";
+    }
+
+    @BeforeEach
+    public void setUpOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
     }
 
     private void provideInput(String data) {
@@ -23,21 +33,62 @@ class ShortLinkControllerTest {
         System.setIn(testIn);
     }
 
-    @BeforeEach
-    public void init() {
-        System.setOut(new PrintStream(outputStreamCaptor));
+    private String getOutput() {
+        return testOut.toString();
     }
 
     @AfterEach
-    public void clean() {
-        System.setOut(standardOut);
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
     }
 
     @Test
-    void getShortLinkWithNoKeywordTest() {
+    void welcomeMessageTest() {
+        provideInput("");
+        new ShortLinkController();
+        Assertions.assertTrue(getOutput().indexOf(ShortLinkMessage.welcomeMessage) > -1);
+    }
+
+    @Test
+    void argumentValidTest() {
+        provideInput("too many arguments");
+        new ShortLinkController();
+        Assertions.assertTrue(getOutput().indexOf(ShortLinkMessage.argumentValid) > -1);
+    }
+
+    @Test
+    void urlValidWithWrongURLTest() {
+        provideInput("wrong_url");
+        new ShortLinkController();
+        Assertions.assertTrue(getOutput().indexOf(ShortLinkMessage.urlValid) > -1);
+    }
+
+    @Test
+    void urlValidWithCorrectURLTest() {
         provideInput(url);
-        final ShortLinkController shortLinkController = new ShortLinkController();
-        shortLinkController.getShortLinkWithNoKeyword(url);
+        new ShortLinkController();
+        Assertions.assertTrue(getOutput().indexOf(ShortLinkService.host) > -1);
+    }
+
+    @Test
+    void keywordValidWithForbiddenSymbolsTest() {
+        provideInput(url + " ~+000");
+        new ShortLinkController();
+        Assertions.assertTrue(getOutput().indexOf(ShortLinkMessage.keywordValid) > -1);
+    }
+    @Test
+    void keywordValidWithWrongNumberOfArgumentsTest() {
+        provideInput(url + " 1");
+        new ShortLinkController();
+        Assertions.assertTrue(getOutput().indexOf(ShortLinkMessage.keywordValid) > -1);
+    }
+
+    @Test
+    void keywordValidWithCorrectKeywordTest() {
+        provideInput(url + " abc12");
+        new ShortLinkController();
+        Assertions.assertTrue(getOutput().indexOf(ShortLinkService.host) > -1);
     }
 
 }
